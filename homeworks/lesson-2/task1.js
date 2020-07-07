f15a018941f3621cb195cc160a738c929c4382ec
BorislavSazz <63195701+BorislavSazz@users.noreply.github.com>
2020-07-07 14:18:20 +0300

    Add files via upload

---
+++ homeworks/lesson-2/task1.js
@@ -0,0 +1,15 @@
+let lang = prompt("ru or en?");
+
+switch (lang){
+
+case 'ru':
+alert('Вы выбрали русский язык');
+break;
+
+case 'en':
+alert('Вы выбрали английский язык');
+break;
+
+default:
+alert('Вы выбрали украинский язык');
+}
