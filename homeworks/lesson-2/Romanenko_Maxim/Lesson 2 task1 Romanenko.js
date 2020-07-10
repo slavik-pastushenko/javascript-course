e54ce821c5769ceb3e0f15a3069c941abcda7814
aximus4206 <67800228+aximus4206@users.noreply.github.com>
2020-07-10 21:16:24 +0300

    add homework

---
+++ homeworks/lesson-2/Romanenko_Maxim/Lesson 2 task1 Romanenko.js
@@ -0,0 +1,13 @@
+let language = prompt ("Choose language (EN or RU)");
+    switch (language){
+        case 'EN':
+            alert ('English chosen!');
+            break;
+
+        case 'RU':
+            alert ('Russian chosen!');
+            break;
+
+        default:
+            alert ('Primary language - Russian');
+    }
