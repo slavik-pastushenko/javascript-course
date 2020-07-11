656b4cdb9cd373f51eaf8306ca9a497a9b2e42d9
Valerian <67800189+valspi@users.noreply.github.com>
2020-07-11 14:49:25 +0300

    Create task.1.js

---
+++ homeworks/lesson-2/Spitsyna_Valeria/task.1.js
@@ -0,0 +1,15 @@
+
+const language = prompt();
+
+switch (language) 
+{
+    case "en":
+        console.log('selected' + ' ' + language)
+        break;
+    case "ru":
+        console.log('selected' + ' ' + language)
+        break;
+    default:
+        console.log('ukr');
+}
+
