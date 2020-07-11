4e767bf3aab5a498422cc4700ed155e01256c925
Valerian <67800189+valspi@users.noreply.github.com>
2020-07-11 14:50:06 +0300

    Create task.2.js

---
+++ homeworks/lesson-2/Spitsyna_Valeria/task.2.js
@@ -0,0 +1,13 @@
+
+const min = 25;
+
+if (min >= 0 && min <= 15){
+    alert("part one");
+} else if (min > 15 && min <= 30){
+    alert("part two");
+} else if (min > 30 && min <= 45){
+    alert("part three");
+} else (min > 45 && min <= 59){
+    alert("part four");
+}
+
