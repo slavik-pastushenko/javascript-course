710a648529ca6477ca7c9d27d6260d4299d99175
Святослав Леонов <appletrollface@gmail.com>
2020-07-14 19:47:13 +0300

    2nd commit with some changes

--- homeworks/lesson-4/Leonov_Svyatoslav/task2.js
+++ homeworks/lesson-4/Leonov_Svyatoslav/task2.js
@@ -1,7 +1,7 @@
 let array = [1, 2, 5, 9, 4, 13, 4, 10];
 
-for (x = 1; x <= array.length; x++){
-    if (array[x - 1] === 4){
+for (x = 0; x <= array.length; x++){
+    if (array[x] === 4){
         console.log('Есть!');
         break;
     }
