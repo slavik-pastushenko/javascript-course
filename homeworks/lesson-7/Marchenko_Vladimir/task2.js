8c0a71a36dea6b70925d200dc3f332f7399ddcaa
Vladimir Marchenko <volodia04041996@gmail.com>
2020-07-30 20:08:23 +0300

    Add HW 7

---
+++ homeworks/lesson-7/Marchenko_Vladimir/task2.js
@@ -0,0 +1,24 @@
+function arrayPromise (arr) {
+	return new Promise((resolve, reject) => arr.some(elem => elem === null) === true ? 
+		setTimeout(() => reject(), 3000) : 
+		setTimeout(() => resolve(), 3000))
+}
+
+const arr2 = [1, 2, null, 3, 2];
+
+arrayPromise(arr2).then(() => console.log('нету null')).catch(() => console.log('есть null'));
+
+// solution 2
+
+async function arrayPromise2 (arr3) {
+	let arrPromises = new Promise((resolve, reject) => arr3.some(elem => elem === null) === false ? 
+		setTimeout(() => resolve('array dont have null'), 1000) : 
+		setTimeout(() => reject('null in array'), 1000)); 
+	let awwPromise = await arrPromises;
+	return awwPromise
+}
+
+
+
+const arr4 = [1, 2, null, 3, 2];
+arrayPromise2(arr4);
