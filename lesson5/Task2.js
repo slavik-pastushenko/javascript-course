7eb4c884ffd51615a7ce4ff05bd100e7bea6cb8b
Dimalis24 <67799855+Dimalis24@users.noreply.github.com>
2020-08-02 20:51:21 +0300

    Lesson #5

---
+++ lesson5/Task2.js
@@ -0,0 +1,8 @@
+const arr1 = [4, 2, 5, 19, 13, 0, 10];
+let Sum3 = 0;
+for (i = 0; i < arr1.length; i++){
+    Sum3 += Math.pow(arr1[i], 3);
+}
+console.log(Math.sqrt(Sum3));
+
+
---
+++ lesson5/Task3.js
@@ -0,0 +1,6 @@
+const arr2 = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+
+let arr = arr2[1].find(item => item == 4);
+
+console.log(arr);
+
