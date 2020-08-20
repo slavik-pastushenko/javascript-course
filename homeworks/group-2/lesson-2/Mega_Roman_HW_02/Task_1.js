c44ae3fedbfeca775af28e786e78050b28afc42b
Roman <megaromankh@gmail.com>
2020-08-20 21:07:48 +0300

    create homework

---
+++ homeworks/group-2/lesson-2/Mega_Roman_HW_02/Task_1.js
@@ -0,0 +1,11 @@
+var language = prompt ('Use language ru, en');
+switch (language) {
+    case "ru":
+        console.log('language = ru');
+        break;
+    case "en":
+        console.log('language = en');
+        break;
+    default:
+        console.log('language = ua');     
+}
---
+++ homeworks/group-2/lesson-2/Mega_Roman_HW_02/Task_3.js
@@ -0,0 +1,3 @@
+let count = +prompt('0 || 2');
+let result = (+count === 0 || +count === 2) ? +count + 7 : +count / 10;
+console.log(result)
