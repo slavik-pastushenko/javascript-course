d14345a1f33897c6b2577692378873ae7eaea612
SuvorovLemon <69644275+SuvorovLemon@users.noreply.github.com>
2020-08-24 12:50:15 +0300

    correct task3

--- homeworks/group-2/lesson-3/SUVOROV_RUSLAN/task3.js
+++ homeworks/group-2/lesson-3/SUVOROV_RUSLAN/task3.js
@@ -2,19 +2,24 @@ const manipulation = (firstNumber, secondNumber) => {
     a=firstNumber%2;
     b=secondNumber%2;
     if (a==0 && b==0) {
-        console.log(firstNumber*secondNumber);
+        return firstNumber*secondNumber;
     }
     else if (a>0 && b>0){
-        console.log(firstNumber+secondNumber);
+        return firstNumber+secondNumber;
     }
     else if (a>0 && b==0){
-        console.log(firstNumber);
+        return firstNumber;
     }
     else{
-        console.log(secondNumber);
+        return secondNumber;
     };
 };
-manipulation(4,4);
-manipulation(3,5);
-manipulation(2,5);
-manipulation(3,8);
+//вызов и проверка:
+a1 = manipulation(4,4);
+b1 = manipulation(3,5);
+c1 = manipulation(2,5);
+d1 = manipulation(3,8);
+console.log(a1);
+console.log(b1);
+console.log(c1);
+console.log(d1);
