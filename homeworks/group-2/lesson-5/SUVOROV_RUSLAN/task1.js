5aab6692737aed0cc3a219e5c4881abfa4954519
SuvorovLemon <69644275+SuvorovLemon@users.noreply.github.com>
2020-08-30 20:22:13 +0300

    Update task1.js

--- homeworks/group-2/lesson-5/SUVOROV_RUSLAN/task1.js
+++ homeworks/group-2/lesson-5/SUVOROV_RUSLAN/task1.js
@@ -4,4 +4,4 @@ while(thousand > 50){
     thousand /= 2;
     totalIterations++;
 }
-console.log(`Resoult = ${thousand}, totalIterations = ${totalIterations}`);
+console.log(`Result = ${thousand}, totalIterations = ${totalIterations}`);
