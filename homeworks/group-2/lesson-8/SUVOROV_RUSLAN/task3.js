0081749e8fbf10bf2e3d475f58cb42ed1e853224
SuvorovLemon <69644275+SuvorovLemon@users.noreply.github.com>
2020-09-09 21:02:54 +0300

    add task3.js

---
+++ homeworks/group-2/lesson-8/SUVOROV_RUSLAN/task3.js
@@ -0,0 +1,3 @@
+Boolean.prototype.toString = function() {return ''+this;}
+Number.prototype.toString = function() {return ''+this;}
+Array.prototype.toString = function() {return '['+this.join(',')+']';}
