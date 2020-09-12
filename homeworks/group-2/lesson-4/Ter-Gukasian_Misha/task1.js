c01853c190796dcacdebdc9c0d6ce02ebd1123db
misha <mter1982@gmail.com>
2020-09-12 16:12:58 +0300

    hw_4

--- homeworks/group-2/lesson-4/Ter-Gukasian_Misha/task1.js
+++
@@ -1,4 +0,0 @@
-let words = ['Honda', 'AlfaRomeo', 'Mercedes', 'Audiiiii', 'BMW', 'Toyota', 'Ford'];
-let wordsBig = words.filter(words => words.length > 5);
-words = wordsBig
-console.log(words)
--- homeworks/group-2/lesson-4/Ter-Gukasian_Misha/task3.js
+++
@@ -1,7 +0,0 @@
-const arr =[1, 2, 3, 4, 5];
-let car = arr.map((function(num){
-    if (num === 1) return `${num } машина`;
-if (num  === 5) return `${num } машин`;
-if (num  > 1 && num  < 5) return `${num } машины`
-}))
-console.log(car);
