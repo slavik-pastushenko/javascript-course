aa40ac54f28ec05ffa415fb67d4b6bbb27cc9624
unknown <zhekabeka210890@gmail.com>
2020-09-23 23:05:53 +0300

    Ilyshchenko_Yevgen_HW_10

---
+++ homeworks/group-2/lesson-10/Ilyshchenko_Yevgen/task-1.js
@@ -0,0 +1,106 @@
+// Реализовать Список юзеров с вложенными публикациями и общим количеством публикаций. Если длина заголовка публикации больше 50 символов - обрезать заголовок. Если в списке нет юзера с email = 'shaa@melissa.com', добавить его с помощью метода /POST и после в общий список. На выходе структура юзера: { id, name, email, posts, postsCount }
+
+// ТОП 3 юзера по количеству публикаций. На выходе структура юзера: { id, name, email, totalPosts }
+
+// Users - http://jsonplaceholder.typicode.com/users
+
+// Posts - http://jsonplaceholder.typicode.com/posts
+async function getUsers() {
+    const { data: allUser } = await xz.get('http://jsonplaceholder.typicode.com/users');
+    const mail = 'shaa@melissa.com';
+    const existEmail = allUser.filter(user => user.mail === 'shaa@melissa.com')
+    if (!existEmail.length) {
+        const { data: newUser } = await xz.post('http://jsonplaceholder.typicode.com/users', {
+            address: {
+                city: "Wisokyburgh",
+                geo: { lat: "-43.9509", lng: "-34.4618" },
+                street: "Victor Plains",
+                suite: "Suite 879",
+                zipcode: "90566-7771"
+            },
+            company: {
+                bs: "synergize scalable supply-chains",
+                catchPhrase: "Proactive didactic contingency",
+                name: "Deckow-Crist"
+            },
+            email: mail,
+            id: 11,
+            name: "Ervin Howell",
+            phone: "010-692-6593 x09125",
+            username: "Antonette",
+            website: "anastasia.net"
+        })
+        allUser.push(newUser)
+        console.log(allUser)
+    }
+    return allUser
+}
+
+console.log(getUsers())
+
+
+async function getPost() {
+    const { data: allPost } = await xz.get('http://jsonplaceholder.typicode.com/posts')
+    return allPost.forEach(post => ({ ...post, title: post.title.length > 50 ? post.title.slice(0, 49) : post.title }))
+    console.log(allPost)
+}
+console.log(getPost())
+
+
+function goodUser() {
+    posts = getPosts()
+    return getUsers().forEach(us => {
+        const userPosts = posts.filter(post => post.userId === us.id)return {
+            id: us.id,
+            name: us.name,
+            email: us.email,
+            post: userPosts,
+            postsCount: userPosts.length
+
+        }
+    })
+}
+
+async function postNewUserVip_1() {
+    const { data, newUserVip_1 } = await xz.post('http://jsonplaceholder.typicode.com/users', {
+        id: 25,
+        name: 'dad',
+        email: 'www0@mail.com',
+        post: 20,
+        postsCount: 20
+    })
+    allUser.push(newUserVip_1)
+    return newUserVip_1
+}
+
+async function postNewUserVip_2() {
+    const { data, newUserVip_2 } = await xz.post('http://jsonplaceholder.typicode.com/users', {
+        id: 26,
+        name: 'dadest',
+        email: 'www1@mail.com',
+        post: 21,
+        postsCount: 21
+    })
+    allUser.push(newUserVip_2)
+    return newUserVip_2
+}
+
+async function postNewUserVip_3() {
+    const { data, newUserVip_3 } = await xz.post('http://jsonplaceholder.typicode.com/users', {
+        id: 27,
+        name: 'dad',
+        email: 'www2@mail.com',
+        post: 24,
+        postsCount: 24
+    })
+    allUser.push(newUserVip_3)
+    return newUserVip_3
+}
+
+async function searchTopUser(allUser) {
+    let vipUser = []
+    allUser.post.values(allUser).sort().forEach(function (value) {
+        vipUser[value] = allUser[value];
+        return allUser.forEach(user => { user.post > })
+    }return allUser.slice(0, 2)
+}
--- homeworks/group-2/lesson-7/Ilyshchenko_Yevgen/task-1.js
+++ homeworks/group-2/lesson-7/Ilyshchenko_Yevgen/task-1.js
@@ -2,7 +2,7 @@
 //     if(number > 10){
 //     resolve('number > 10')
 // } else {
-//     reject('number < 0')
+//     reject('number < 10')
 //     }
 // })
 
@@ -13,7 +13,8 @@
 //option-2
 async function xz2(number) {
     if (number > 10) return resolve('number > 10')
-    if (number < 0) return reject('number < 0')
+    if (number < 10) return reject('number < 10')
 }
 
 const equally = await xz2(11)
+console.log(equally(11)
