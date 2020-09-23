c8c0f2092b202f54229bdd2226b6d8479615e1f2
unknown <zhekabeka210890@gmail.com>
2020-09-23 23:07:42 +0300

    Ilyshchenko_Yevgen_HW_10

--- homeworks/group-2/lesson-7/Ilyshchenko_Yevgen/task-1.js
+++ homeworks/group-2/lesson-7/Ilyshchenko_Yevgen/task-1.js
@@ -2,7 +2,7 @@
 //     if(number > 10){
 //     resolve('number > 10')
 // } else {
-//     reject('number < 10')
+//     reject('number < 0')
 //     }
 // })
 
@@ -13,8 +13,7 @@
 //option-2
 async function xz2(number) {
     if (number > 10) return resolve('number > 10')
-    if (number < 10) return reject('number < 10')
+    if (number < 0) return reject('number < 0')
 }
 
 const equally = await xz2(11)
-console.log(equally(11)
