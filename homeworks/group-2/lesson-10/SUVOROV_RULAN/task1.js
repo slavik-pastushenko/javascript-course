358fe9a7b33f5d5533eb1a3bbbd7984fa41a09bf
SuvorovLemon <69644275+SuvorovLemon@users.noreply.github.com>
2020-09-29 15:39:18 +0300

    add second variant

--- homeworks/group-2/lesson-10/SUVOROV_RULAN/task1.js
+++ homeworks/group-2/lesson-10/SUVOROV_RULAN/task1.js
@@ -55,3 +55,53 @@ function getTopThreeUsets(){
     });
 }
 console.log (getTopThreeUsets())
+
+//Worked
+const axios = require('axios');
+
+    const {data: users} = await axios.get('http://jsonplaceholder.typicode.com/users');
+
+    const isEmailExists = users.filter(user => user.email === 'shaa@melissa.com');
+
+    if (!isEmailExists.length) {
+        const { data: newUser } = await axios.post('http://jsonplaceholder.typicode.com/users', {
+            "name":"Shaa Melissa",
+            "email":"shaa@melissa.com"
+        });
+        users.push(newUser);
+        }
+    const { data: posts } = await axios.get('http://jsonplaceholder.typicode.com/posts');
+    
+    posts.map(post => ({
+        ...post,
+        title: post.title.length > 50
+        ? post.title.slice(0, 49)
+        : post.title
+    }));
+
+    const usersListWithPosts = users.map(user => {
+        const userPosts = posts.filter(post => post.userId === user.id);
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            posts: userPosts,
+            postsCount: userPosts.length
+        };
+    });
+console.log (usersListWithPosts)
+
+function getTopThreeUsets(){
+    return usersListWithPosts
+    .sort((a,b) => a.postsCount<b.postsCount ? 1 : -1)
+    .splice(3)  //на runkit не обрезает почему-то
+    .map(user=>{
+        return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        totalPosts: user.postsCount
+        }
+    });
+}
+console.log (getTopThreeUsets())
